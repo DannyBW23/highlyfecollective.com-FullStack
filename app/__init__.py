d0e96e75832e13297cca725efb2fbf26cc3f5b5b
DannyBW23 <140914543+DannyBW23@users.noreply.github.com>
2023-07-30 12:08:23 -0400

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,80 @@
+import logging
+from logging.handlers import SMTPHandler,RotatingFileHandler
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_wtf.csrf import CSRFProtect
+from flask_ckeditor import CKEditor  
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from config import Config
+
+
+db = SQLAlchemy()#removed app
+migrate = Migrate()#removed app,db
+login = LoginManager()#removed app
+login.login_message = ''
+login.login_view= 'auth.login'
+mail = Mail() #removed app
+ckeditor = CKEditor()#removed app
+csrf = CSRFProtect()#removed app
+bootstrap=Bootstrap()#removed app
+moment = Moment()#removed app
+UPLOAD_FOLDER = '/Users/danielweir/highlyfe3/app/static/images'
+
+
+def create_app(config_class=Config):
+	app = Flask(__name__)
+	app.config.from_object(config_class)
+	db.init_app(app)
+	migrate.init_app(app, db)
+	login.init_app(app)
+	mail.init_app(app)
+	csrf.init_app(app)
+	ckeditor.init_app(app)
+	bootstrap.init_app(app)
+	moment.init_app(app)
+	app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+
+
+
+	from app.errors import bp as errors_bp
+	app.register_blueprint(errors_bp)
+
+	from app.auth import bp as auth_bp
+	app.register_blueprint(auth_bp, url_prefix='/auth')
+
+	from app.main import bp as main_bp
+	app.register_blueprint(main_bp)
+
+
+	if not app.debug and not app.testing:
+		if app.config['MAIL_SERVER']:
+			auth = None
+			if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+				auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+			secure = None
+			if app.config['MAIL_USE_TLS']:
+				secure = ()
+			mail_handler = SMTPHandler(mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),fromaddr='no-reply@' + app.config['MAIL_SERVER'],toaddrs=app.config['ADMINS'], subject='Highlyfe Failure',credentials=auth, secure=secure)
+			mail_handler.setLevel(logging.ERROR)
+			app.logger.addHandler(mail_handler)
+
+		if not os.path.exists('logs'):
+			os.mkdir('logs')
+		file_handler = RotatingFileHandler('logs/Highlyfe.log', maxBytes=10240, backupCount=10)	
+		file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+		file_handler.setLevel(logging.INFO)
+		app.logger.addHandler(file_handler)
+		app.logger.setLevel(logging.INFO)
+		app.logger.info('Highlyfe startup')
+	return app
+
+
+from app import models #errors, routes
+
---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+#hey
+class Config(object):
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'clemsonite'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+	LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	MAIL_SERVER = os.environ.get('MAIL_SERVER')
+	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+	ADMINS = ['dannybw212@gmail.com']
+	POSTS_PER_PAGE = 50
