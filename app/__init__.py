ce5c31a7176de4d920fe9cb5fa4301236beee1e0
DannyBW23 <dannybw212@gmail.com>
2023-08-01 11:44:10 -0400

    run

--- app/__init__.py
+++ app/__init__.py
@@ -23,11 +23,11 @@ ckeditor = CKEditor()#removed app
 csrf = CSRFProtect()#removed app
 bootstrap=Bootstrap()#removed app
 moment = Moment()#removed app
-UPLOAD_FOLDER = '/Users/danielweir/highlyfe3/app/static/images'
-
+from flask_talisman import Talisman
 
 def create_app(config_class=Config):
 	app = Flask(__name__)
+	Talisman(app, content_security_policy=None)
 	app.config.from_object(config_class)
 	db.init_app(app)
 	migrate.init_app(app, db)
