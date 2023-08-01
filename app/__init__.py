b639013316bb02f2d7e4b6d2d8b2dd2ec5a5c6d3
DannyBW23 <dannybw212@gmail.com>
2023-08-01 12:27:21 -0400

    test

--- app/__init__.py
+++ app/__init__.py
@@ -50,6 +50,7 @@ def create_app(config_class=Config):
 
 
 	if not app.debug and not app.testing:
+		print('hey')
 		if app.config['MAIL_SERVER']:
 			auth = None
 			if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
