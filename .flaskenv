20a03e21272b34d1bdb521975eda6f29aa04cdf5
DannyBW23 <dannybw212@gmail.com>
2023-08-03 13:45:03 -0400

    ju

--- .flaskenv
+++ .flaskenv
@@ -1,8 +1,9 @@
-FLASK_APP=highlyfe3.py
-MAIL_SERVER=smtp.mailgun.org
-MAIL_PORT=587
-MAIL_USE_TLS=1
-MAIL_USERNAME=postmaster@highlyfecollective.com
-MAIL_PASSWORD=878aecdef6211dfa52916cad8f04f616-4e034d9e-de17caf4
-MAIL_AUTHENTICATION='Plain login'
+# FLASK_APP=highlyfe3.py
+# MAIL_SERVER=smtp.mailgun.org
+# MAIL_PORT=587
+# MAIL_USE_TLS=1
+MAILGUN_KEY=88d3d636fe020ca0b81cd506dc52635a-4e034d9e-6046f5a2
+MAILGUN_DOMAIN=postmaster@highlyfecollective.com
+# MAIL_PASSWORD=878aecdef6211dfa52916cad8f04f616-4e034d9e-de17caf4
+# MAIL_AUTHENTICATION='Plain login'
 
--- app/auth/email.py
+++ app/auth/email.py
@@ -1,7 +1,15 @@
 from flask import render_template, current_app
-from app.email import send_email
-def send_password_reset_email(user):
-	token = user.get_reset_password_token()
-	send_email('[Highlyfe] Reset Your Password',sender=current_app.config['MAIL_USERNAME'],recipients=[user.email],text_body=render_template('email/reset_password.txt',user=user, token=token),html_body=render_template('email/reset_password.html',user=user, token=token))
+import requests
+def send_password_reset_email(to_address, from_address, subject, plaintext, html):
+	r = requests.post("https://api.mailgun.net/v2/%s/messages" % current_app.config['MAILGUN_DOMAIN'],auth=("api", current_app.config['MAILGUN_KEY']),data={"from": from_address, "to": to_address, "subject": subject,"text": plaintext, "html": html})
+	return r
+ 
+
+def send_mail(user):
+    token = user.get_reset_password_token()
+    send_password_reset_email('[Highlyfe] Reset Your Password',sender=current_app.config['MAIL_USERNAME'],recipients=[user.email],text_body=render_template('email/reset_password.txt',user=user, token=token),html_body=render_template('email/reset_password.html',user=user, token=token))
+ 
+ # from app.email import send_email 
+# send_email('[Highlyfe] Reset Your Password',sender=current_app.config['MAIL_USERNAME'],recipients=[user.email],text_body=render_template('email/reset_password.txt',user=user, token=token),html_body=render_template('email/reset_password.html',user=user, token=token))
 
 
