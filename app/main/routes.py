4bffde91d47a315ea28a5aab0ea7d2774814c199
DannyBW23 <dannybw212@gmail.com>
2023-08-11 17:01:25 -0400

    lets go

--- app/main/routes.py
+++ app/main/routes.py
@@ -365,16 +365,30 @@ def send_message(recipient):
 		flash(('Your message has been sent.'))
 		return redirect(url_for('main.user', username=recipient))
 	return render_template('send_message.html', title=('Send Message'),form=form, recipient=recipient)
-@bp.route('/messages')
+@bp.route('/send_message1/<recipient>', methods=['GET', 'POST'])
 @login_required
-def messages():
+def send_message1(recipient):
+	user = User.query.filter_by(username=recipient).first_or_404()
+	form = MessageForm()	
+	if form.validate_on_submit():
+		msg = Message(author=current_user, recipient=user,body=form.message.data)
+		db.session.add(msg)
+		user.add_notification('unread_message_count', user.new_messages())
+		db.session.commit()
+		flash(('Your message has been sent.'))
+		return redirect(url_for('main.messages', username=recipient))
+	return render_template('send_message.html', title=('Send Message'),form=form, recipient=recipient)
+@bp.route('/messages/<username>')
+@login_required
+def messages(username):
 	current_user.last_message_read_time = datetime.utcnow()
 	current_user.add_notification('unread_message_count', 0)
 	db.session.commit()
+	user = User.query.filter_by(username=username).first_or_404()
 	page = request.args.get('page', 1, type=int)
 	messages = current_user.messages_received.order_by(Message.timestamp.desc()).paginate(page=page, per_page=current_app.config['POSTS_PER_PAGE'],error_out=False)
-	next_url = url_for('main.messages',page=messages.next_num)if messages.has_next else None
-	prev_url = url_for('main.messages',page=messages.prev_num)if messages.has_prev else None
+	next_url = url_for('main.messages',username=user.username,page=messages.next_num)if messages.has_next else None
+	prev_url = url_for('main.messages',username=user.username,page=messages.prev_num)if messages.has_prev else None
 	return render_template('messages.html', messages=messages.items,next_url=next_url, prev_url=prev_url, user=user)
 @bp.route('/notifications')
 @login_required
