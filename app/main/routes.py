b2eea7240029e82f82699882dfc3f44fadf21b53
DannyBW23 <dannybw212@gmail.com>
2023-11-03 22:20:30 -0400

    m

--- app/main/routes.py
+++ app/main/routes.py
@@ -13,22 +13,23 @@ from botocore.exceptions import ClientError
 from sqlalchemy import func
 from app.auth.forms import LoginForm
 import os
-@bp.before_app_request
-def before_request():
-	if not request.is_secure:
-		if not current_user.is_authenticated:
-			url = request.url.replace("http://", "https://", 1)
-			return redirect(url, code=301)
-	if not request.is_secure:
-		if current_user.is_authenticated:
-			url = request.url.replace("http://", "https://", 1)
-			current_user.last_seen = datetime.utcnow()
-			db.session.commit()
-			return redirect(url, code=301)
-	if request.is_secure:
-		if current_user.is_authenticated:
-			current_user.last_seen = datetime.utcnow()
-			db.session.commit()	
+# @bp.before_app_request
+# def before_request():
+# 	if not request.is_secure:
+# 		if not current_user.is_authenticated:
+# 			url = request.url.replace("http://", "https://", 1)
+# 			return redirect(url, code=301)
+# 	if not request.is_secure:
+# 		if current_user.is_authenticated:
+# 			url = request.url.replace("http://", "https://", 1)
+# 			current_user.last_seen = datetime.utcnow()
+# 			db.session.commit()
+# 			return redirect(url, code=301)
+# 	if request.is_secure:
+# 		if current_user.is_authenticated:
+# 			current_user.last_seen = datetime.utcnow()
+# 			db.session.commit()	
+			
 @bp.route('/studio', methods=['GET','POST'])
 @login_required
 def studio():
@@ -474,8 +475,7 @@ def index():
 						s3_client.upload_fileobj(file, 'profilepic23', pic_name)
 						name_to_update.pitx = pic_name 
 						db.session.commit()
-						return redirect(url_for('main.index', form=form))
-					except ClientError as e:
+						except ClientError as e:
 						print(f"Error uploading file to AWS S3: {e}")
 						flash("Error!  Looks like there was a problem...try again!")
 						return render_template("index.html", name_to_update=name_to_update, id=id, form=form)
@@ -1077,7 +1077,6 @@ def pin_post(id):
 	else:
 		flash("You are not authorized to pin this post.")
 	return redirect(url_for('main.explore'))
-                
 @bp.route('/posts/unpin/<int:id>', methods=['POST'])
 @login_required
 def unpin_post(id):
